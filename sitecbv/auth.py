6127387f40ddb6af9ceadca5795ab4e95af29e81
Felipe Gomes Vieira Ferreira <felipe.ferreira@mapptv.com.br>
2017-05-23 14:24:41 -0300

    finalizacao processo login

--- sitecbv/auth.py
+++ sitecbv/auth.py
@@ -2,5 +2,13 @@ from allauth.account.adapter import DefaultAccountAdapter
 
 
 class AccountAdapter(DefaultAccountAdapter):
-    pass
+    def get_login_redirect_url(self, request):
+        print "um"
+        return None
+    def get_logout_redirect_url(self, request):
+        print "dois"
+        return None
+    def get_email_confirmation_redirect_url(self, request):
+        print "tres"
+        return None
 
