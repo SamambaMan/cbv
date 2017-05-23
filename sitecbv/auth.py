881ac58bbbd36e39910aca58a99a8ded494df228
Felipe Gomes Vieira Ferreira <felipe.ferreira@mapptv.com.br>
2017-05-23 15:35:46 -0300

    alteracoes para melhorar o fluxo de login e login social

--- sitecbv/auth.py
+++ sitecbv/auth.py
@@ -1,14 +1,28 @@
 from allauth.account.adapter import DefaultAccountAdapter
 
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class AccountAdapter(DefaultAccountAdapter):
-    def get_login_redirect_url(self, request):
-        print "um"
-        return None
-    def get_logout_redirect_url(self, request):
-        print "dois"
-        return None
-    def get_email_confirmation_redirect_url(self, request):
-        print "tres"
-        return None
+    pass
+    #def get_login_redirect_url(self, request):
+    #    print "um"
+    #    return "/"
+    #def get_logout_redirect_url(self, request):
+    #    print "dois"
+    #    return None
+    #def get_email_confirmation_redirect_url(self, request):
+    #    print "tres"
+    #    return None
 
+
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+    pass
+    #def get_login_redirect_url(self, request):
+    #    return "/"
+    #def get_logout_redirect_url(self, request):
+    #    print "dois"
+    #    return None
+    #def get_email_confirmation_redirect_url(self, request):
+    #    print "tres"
+    #    return None
